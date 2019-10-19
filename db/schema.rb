d01d4612b0e25996c78af10a4f5df6b777bca5fd
hpjaj <hmlevine@gmail.com>
2019-10-19 15:57:35 -0600

    Related posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_212419) do
+ActiveRecord::Schema.define(version: 2019_10_19_213134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_10_19_212419) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
